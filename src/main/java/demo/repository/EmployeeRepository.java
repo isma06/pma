fc53c0aae87a3fd83d734ea2b426d4c774ed727a
unknown <poo2@PUEPUE-AD6PH00K.aula.local>
2015-06-17 16:11:17 +0200

    Initial commit

---
+++ src/main/java/demo/repository/EmployeeRepository.java
@@ -0,0 +1,19 @@
+package demo.repository;
+
+import demo.model.Employee;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by poo2 on 16/06/2015.
+ */
+public interface EmployeeRepository extends PagingAndSortingRepository <Employee, Long> {
+     List<Employee> findByNameAndSurname( @Param("name") String name,
+                                          @Param("surname") String surname);
+
+     List<Employee> findByDateIncorporationBetween (@Param("start") Date start, @Param("end") Date end);
+}
---
+++ src/main/java/demo/service/EmployeeService.java
@@ -0,0 +1,61 @@
+package demo.service;
+
+import demo.model.Employee;
+import demo.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by poo2 on 16/06/2015.
+ */
+
+@Service
+public class EmployeeService {
+    
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    public void testEmployees(){
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2015, calendar.JANUARY, 1);
+        Date startDate = calendar.getTime();
+
+        calendar.set(2015, calendar.MARCH, 1);
+        Date endDate= calendar.getTime();
+
+        calendar.set(2015, calendar.FEBRUARY, 1);
+        Date dateIncor = calendar.getTime();
+
+
+        Employee employee = new Employee();
+        employee.setName("Arturo");
+        employee.setSurname("El Kabouri");
+        employee.setSalary(2400.0);
+        employee.setDateIncorporation(new Date());
+
+        employeeRepository.save(employee);
+
+
+        Employee employee2 = new Employee();
+        employee2.setName("Ismail");
+        employee2.setSurname("El Kabouri");
+        employee2.setSalary(3400.0);
+        employee2.setDateIncorporation(dateIncor);
+
+        employeeRepository.save(employee2);
+
+        List<Employee> list = employeeRepository.findByNameAndSurname("Ismail", "El Kabouri");
+
+        for(Employee em : list){
+            System.out.println(em.toString());
+        }
+
+
+        Employee ismail = employeeRepository.findByDateIncorporationBetween(startDate, endDate).get(0);
+        System.out.println(ismail.toString());
+    }
+}
